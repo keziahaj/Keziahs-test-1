4d74d4322eca777a6e29441a2f5efdd5efbb7512
keziahaj <keishaajufo@gmail.com>
2022-09-24 14:20:52 +0100

    homework Q2 and Q5

--- array_checker .py
+++ array_checker .py
@@ -3,6 +3,10 @@
 # 21/09/22
 
 numbers_list = []
+average = 0 
+
+def main():
+    print(numbers_list)
 
 def validation():
     choice = int(input("How many numbers would you like to input? Please enter an amount above 2."))
@@ -18,9 +22,36 @@ def numbers(choice, numbers_list):
         print(numbers_list)
     return numbers_list
 
-def main(validation, numbers):
-    print(numbers_list)
+
+
+def display():
+
+    average = (numbers_list/len(numbers_list))
+    print(average)
+    
+    largest_number = numbers_list[0]
+    for x in numbers_list:
+        if x > largest_number:
+            largest_number = x
+            print(largest_number)
+
+    midpoint = (numbers_list + 1)/2
+    print(midpoint)
 
 main()
+
+ 
+ # A program that identifies if a number is even or odd
+
+number1 = int(input("Enter a number."))
+calc = number1/2
+
+if calc == int:
+    print(number1 + f"is an even number.")
+else:
+    print(number1 + f"is an odd number")
+
+
+
 
 
